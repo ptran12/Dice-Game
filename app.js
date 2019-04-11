9867c3ae95ade79625604a9820656027e57d88fb
Phi Tran <phitran@Phis-MBP.attlocal.net>
2019-04-11 13:17:05 -0500

    game logic is working for both dices and displays who the winner is

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber1);
 
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber2);
+
 var leftDice = document.getElementById("dice1");
 var rightDice = document.getElementById("dice2");
+var mainHeader = document.querySelector("h1");
 
-// generating the image dice link
-function pickImg () {
+// generating the image dice link for the left dice
+function pickImgLeft () {
     var diceImg = "./images/dice" + randomNumber1 + ".png";
     console.log(diceImg);
     return (diceImg);
 }
 
-leftDice.setAttribute("src", pickImg());
+// generating the image dice link for the right dice
+function pickImgRight() {
+  var diceImg = "./images/dice" + randomNumber2 + ".png";
+  console.log(diceImg);
+  return diceImg;
+}
+
+function display () {
+    if (randomNumber1 > randomNumber2) {
+        mainHeader.innerHTML = "Player 1 Wins!";
+    }else if (randomNumber1 < randomNumber2) {
+        mainHeader.innerHTML = "Player 2 Wins!";
+    }else {
+        mainHeader.innerHTML = "It is a draw, refresh the page please!"
+    }
+}
+
+
+leftDice.setAttribute("src", pickImgLeft());
+rightDice.setAttribute("src", pickImgRight());
+display();
 
 
 
